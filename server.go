ff3506a253b201c9b971936155247e837b9cf245
Vlad <svlad1999@ukr.net>
2019-10-01 20:39:22 +0300

    Add NEW server

--- server.go
+++ server.go
@@ -1,25 +1,22 @@
-package main;
+package main
 
-import ("net/http"; "fmt"; "time"; "encoding/json");
-
-func handleRoot(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "text/html");
-	res.Write([]byte("Hello World!"));
-}
-
-func handleTime(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "application/json");
-	jsonTime, err := json.Marshal(map[string]string{ "time": time.Now().Format(time.RFC3339) });
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusInternalServerError);
-		return;
-	}
-	res.Write(jsonTime);
-}
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	http.HandleFunc("/", handleRoot);
-	http.HandleFunc("/time", handleTime);
-	defer http.ListenAndServe(":8795", nil);
-	fmt.Println("Server is listening on 8795 port...");
+	fmt.Println("Server is listening on 8795 port...")
+	http.HandleFunc("/time", HelloServer)
+	http.ListenAndServe(":8795", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	jsonTime, err := json.Marshal(map[string]string{"time": time.Now().Format(time.RFC3339)})
+	if err != nil {
+		return
+	}
+	w.Write(jsonTime)
 }
